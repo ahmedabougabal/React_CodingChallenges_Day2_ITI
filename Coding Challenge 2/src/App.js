d2fc3bf801af464454de36de8b0a98b58c90db39
ahmedabougabal <ahmedabougabal@live.com>
2024-08-02 08:22:33 +0300

    feat : add Count on press functionality

---
+++ Coding Challenge 2/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+const initialProducts = [
+  {
+    id: 0,
+    name: "Baklava",
+    count: 1,
+  },
+  {
+    id: 1,
+    name: "Cheese",
+    count: 5,
+  },
+  {
+    id: 2,
+    name: "Spaghetti",
+    count: 2,
+  },
+];
+
+export default function ShoppingCart() {
+  const [products, setProducts] = useState(initialProducts);
+
+  function handleIncreaseClick(productId) {
+    // your code here
+    const updatedProducts = products.map((item) => {
+      if (productId === item.id) {
+        return {
+          ...item,
+          count: item.count + 1,
+        };
+      } else {
+        return item;
+      }
+    });
+    setProducts(updatedProducts);
+  }
+
+  return (
+    <ul>
+      {products.map((product) => (
+        <li key={product.id}>
+          {product.name} (<b>{product.count}</b>)
+          <button
+            onClick={() => {
+              handleIncreaseClick(product.id);
+            }}
+          >
+            +
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+}
